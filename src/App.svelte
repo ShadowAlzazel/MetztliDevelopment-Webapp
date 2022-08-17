7f304443f044e47cedccf2e1316e5cf87e1cffe7
ShadowAlzazel <pastenalfredojr@gmail.com>
2022-08-16 17:09:41 -0700

    0.1.1

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,17 @@
 <script>
   import svelteLogo from './assets/svelte.svg'
   import Counter from './lib/Counter.svelte'
+
+  let someColor = 'blue'
+
 </script>
 
 <main>
+  <div class="title">
+    <div>
+      <h1 class="h1">Metztli Development</h1>
+    </div>
+  </div>
   <div>
     <a href="https://vitejs.dev" target="_blank"> 
       <img src="/vite.svg" class="logo" alt="Vite Logo" />
@@ -28,6 +36,18 @@
 </main>
 
 <style>
+  .title {
+    background-color: rgb(111, 188, 211);
+    padding: 2.0rem;
+    border-radius: 1.0rem;
+    border-color:  rgb(111, 188, 211);
+  }
+  .title:hover {
+    border-color: rgb(70, 68, 208);
+  }
+  .h1 {
+    color: rgb(70, 68, 208)
+  }
   .logo {
     height: 6em;
     padding: 1.5em;
@@ -39,6 +59,7 @@
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
   }
+
   .read-the-docs {
     color: #888;
   }
